4e644f262a1ded2704e9750a4be84e4e47bd87b6
saikumarerumalla <saikumar.erumalla@gmail.com>
2025-06-09 21:27:32 +0900

    Added the  Delete User API

--- src/main/java/com/explore/Redis_Practice/Service/UserService.java
+++ src/main/java/com/explore/Redis_Practice/Service/UserService.java
@@ -2,6 +2,7 @@ package com.explore.Redis_Practice.Service;
 
 import com.explore.Redis_Practice.Entity.Users;
 import com.explore.Redis_Practice.Repository.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -56,7 +57,7 @@ public class UserService {
                 Users user= userOpt.get();
                 try{
                     String userForCache= objectMapper.writeValueAsString(user);
-                    valueOps.set("user:"+ user.getId(), userForCache);
+                    valueOps.set("user:"+ user.getId(), userForCache, 100, TimeUnit.SECONDS);
                 }catch (Exception e) {
                     log.info("Error : { }", e);
                     return ResponseEntity.status(500).build();
@@ -70,5 +71,32 @@ public class UserService {
     }
 
 
+    public ResponseEntity<String> updateUser(Integer id, Users updatedUser) throws Exception {
+        Optional<Users> userOpt = userRepository.findById(id);
+        if(userOpt.isPresent()){
+            Users existingUser = userOpt.get();
+            existingUser.setName(updatedUser.getName());
+            existingUser.setEmail(updatedUser.getEmail());
+            userRepository.save(existingUser);
 
+            ValueOperations<String, String> valueOps = redisTemplate.opsForValue();
+            String userForCache= objectMapper.writeValueAsString(existingUser);
+            valueOps.set("user:"+ existingUser.getId(), userForCache, 100, TimeUnit.SECONDS);
+
+            return ResponseEntity.ok("User updated in H2 Database and cached in Redis");
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    public ResponseEntity<String> deleteUser(Integer id) {
+        if(userRepository.existsById(id)){
+            userRepository.deleteById(id);
+            redisTemplate.delete("user:"+ id);
+            return ResponseEntity.ok("User deleted from H2 Database and Redis cache");
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
